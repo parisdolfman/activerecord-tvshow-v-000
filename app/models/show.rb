25c925d263bbf392bee9c6a5b5e9a03121e61d9d
Paris Dolfman <parisdolfman@gmail.com>
2020-10-08 18:12:32 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,11 +1,15 @@
 class Show < ActiveRecord::Base
 
   def Show::highest_rating
-    show = Show.maximum(:rating)
+    Show.maximum(:rating)
   end
 
   def Show::most_popular_show
-    show = Show.where("rating = ?", Show.maximum(:rating)).first
+    Show.where("rating = ?", Show.maximum(:rating)).first
   end
+
+  def Show::lowest_rating
+    
+  end 
 
 end
